cba28d9b4660563afb678e68218191fca3240ee6
BOTXavier <xavier.dartevel@gmail.com>
2022-12-06 19:11:15 +0100

    shwo laby fonctionne

--- Projet_info/lab_generator.py
+++ Projet_info/lab_generator.py
@@ -5,13 +5,13 @@ class Case:
     def  __init__(self,x,y):
         self.x = x
         self.y = y
-        self.Nord = False
+        self.North = False
         self.South = False
         self.Est = False
         self.West = False
         self.IsEntrence = False
         self.IsExit = False
-        self.cote=[self.Nord,self.South,self.Est,self.West]
+        self.cote=[self.North,self.South,self.Est,self.West]
 
     def __repr__(self):
         return "Case ("+("N" if self.cote[0] else "") + ("S" if self.cote[1] else "") + ("E" if self.cote[2] else "") + ("W" if self.cote[3] else "") + ")"
@@ -24,26 +24,38 @@ class labyrinth:
 
 
     def show_laby(self):
+        plt.plot([-0.5,self.columns+0.5],[-0.5,-0.5],'black')
+        plt.plot([-0.5,self.columns+0.5],[self.lines+0.5,self.lines+0.5],'black')
+        plt.plot([-0.5,-0.5],[-0.5,self.lines+0.5],'black')
+        plt.plot([self.columns+0.5,self.columns+0.5],[-0.5,self.lines+0.5],'black')
         for i in range(len(self.laby)-1,0,-1):
             for j in range(len(self.laby[i])-1):
                 dir = self.laby[i][j]
-                if  dir.Nord == False:
-                    plt.plot([j, j+1],[i+1, i+1], 'blue')
-                if dir.South == False:
-                    plt.plot([j, j+1], [i, i], 'blue')
-                if dir.Est == False:
-                    plt.plot([j+1, j+1], [i, i+1], 'blue')
-                if dir.West == False:
-                    plt.plot([j, j], [i, i+1], 'blue')
+                if  dir.North == True:
+                    plt.plot([j-0.5, j+0.5],[i+0.5, i+0.5], 'blue')
+                if dir.South == True:
+                    plt.plot([j-0.5, j+0.5], [i-0.5, i-0.5], 'blue')
+                if dir.Est == True:
+                    plt.plot([j+0.5, j+0.5], [i-0.5, i+0.5], 'blue')
+                if dir.West == True:
+                    plt.plot([j-0.5, j-0.5], [i-0.5, i+0.5], 'blue')
         plt.show()
 
 
     def __repr__(self):
-        return f"labyrinth ({self.lines} lines, {self.columns} columns) [{self.lab.__repr__()}]"
+        return f"labyrinth ({self.lines} lines, {self.columns} columns) [{self.laby.__repr__()}]"
 
 
 Lab = labyrinth(5,5)
 Lab.laby[2][0].South = True
+Lab.laby[3][0].Est = True
+Lab.laby[3][1].North = True
+Lab.laby[4][1].West = True
+Lab.laby[0][4].Est = True
+Lab.laby[2][1].North = True
+Lab.laby[1][1].Est = True
+
+print(Lab.laby)
 
 
 Lab.show_laby()
