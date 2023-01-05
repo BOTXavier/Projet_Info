5b31fb3cd740a6c1d3c7d758b78001993359259a
Gonfreecs33 <118904711+Gonfreecs33@users.noreply.github.com>
2023-01-05 14:56:14 +0100

    Add files via upload

---
+++ Projet_info/prioqueue.py
@@ -0,0 +1,32 @@
+import heapq
+
+class PrioQueue(object):
+    def __init__(self, l):
+        """Cree une file a priorite a partir d'une liste l :
+        la liste l doit contenir les couples (priorite, element)"""
+        self.queue = [[prio, i, elt] for (i, (prio, elt)) in enumerate(l)]
+        heapq.heapify(self.queue)
+
+    def __repr__(self):
+        """Representation chaine de la file a priorite"""
+        return '<PrioQueue: {0.queue}>'.format(self)
+
+    def decrease_prio(self, elt, new_prio):
+        """Met a jour la priorite de 'elt' dans la file
+        en la remplacant par 'new_prio'"""
+        for i, (_, _, e) in enumerate(self.queue):
+            if e == elt:
+                self.queue[i][0] = new_prio
+                heapq._siftdown(self.queue, 0, i)
+                return
+        raise KeyError("decrease_prio")
+
+    def pop(self):
+        """Extrait de la file a priorite l'element de priorite minimale
+        Renvoie le couple (priorite, element) correspondant"""
+        prio, _, elt = heapq.heappop(self.queue)
+        return (prio, elt)
+
+    def is_empty(self):
+        """Teste si la file a priorite est vide"""
+        return len(self.queue) == 0
