08e04b669289051a46c5a0c2a088d4e13032f3c2
seck <seckouz33@gmail.com>
2023-01-08 03:23:45 +0100

    show laby réussi

--- Projet_info/pyplot_lab.py
+++ Projet_info/pyplot_lab.py
@@ -5,9 +5,7 @@ import wgraph as g
 def plot_laby(G,t, lab=False):
     
     f = G.size()
-    print(f)
     n = int((f)**(0.5))
-    
     N = [[i+n*j  for i in range(1,1+n)] for j in range(n) ]
     d = 0.5
     plt.axis([-1, n, -1, n])
@@ -24,14 +22,15 @@ def plot_laby(G,t, lab=False):
            j = N[i].index(h)
         except: 
             j = N[i-1].index(h)
+            i=i-1
         try: 
             j1 = N[i1].index(elt)
         except:
             j1 = N[i1-1].index(elt)
+            i1 = i1-1
 
         (x,y) = (i1,j1)
         (a,b) = (i,j)
-        
         if a == x + 1: plt.plot((x+d,x+d),(y-d,y+d),'w')
         elif a == x - 1: plt.plot((x-d,x-d),(y-d,y+d),'w')
         elif b == y + 1: plt.plot((x-d,x+d),(y+d,y+d),'w')
@@ -39,9 +38,9 @@ def plot_laby(G,t, lab=False):
         if lab: plt.plot((x,a), (y,b), 'r')
     plt.show()
 
-G = lg.Laby_Graph(15,0,1) 
+G = lg.Laby_Graph(10,0,1) 
 
 
-t = lg.laby_dict(15,1)
+t = lg.laby_dict(10,1)
 
 plot_laby(G,t,lab=False)
