5a4b0b8b718114d0264442b2067781895f4bef34
YannBernardEnac <119986015+YannBernardEnac@users.noreply.github.com>
2023-01-09 13:08:35 +0100

    Update Laby_generator_wgrah.py

--- Projet_info/Laby_generator_wgrah.py
+++ Projet_info/Laby_generator_wgrah.py
@@ -56,7 +56,45 @@ def Laby_Graph(n,t,w):  # 1<=n<=31 ; t=1 -> random graph ; t=0 -> graph fixe ; w
              G.add_edge(elt,D[elt],random.randint(1,4))
         return G
 
+#ATTENTION ne modifie pas laby dict, juste le graph
+#Le Laby généré précédemment est parfait(=0 cycle), algo choisit un sommet aléatoirement, teste si il lui reste des sommets disponibles puis ajoute le sommet
+#si carré 1,2,3  4,5,6  7,8,9 alors les voisins geographiques de 2 sont 1,3,5
+def Add_Cycles(n,laby,nbCycles):
+    compt,listeVoisinsGraph=0,[]
+    while compt<nbCycles:                                           #répète l'opération m fois pour m cycles
+        nbrandom=random.randint(1,n**2)                             #choisit aléatoirement le sommet sur lequel on veut ajouter une arète
+        condi=False
+        if len(laby.neighbours(nbrandom))< CoinBordOuCentreLaby(n,nbrandom): #regarde si le sommet à moins de voisinsgraph que de voisins possibles(2 pour coin...)
+            for item in laby.neighbours(nbrandom):                  #crée la liste des voisins du sommet
+                listeVoisinsGraph.append(item[0])
+            voisinGeog=voisinGeo(nbrandom,n)
+            while condi==False:
+                random.shuffle(voisinGeog) 
+                if voisinGeog[0] not in listeVoisinsGraph :         #crée un voisin graph si le voisin géographique n'est pas déja dans la liste des voisins graph du sommet
+                    laby.add_edge(nbrandom,voisinGeog[0],1)
+                    condi=True
+            compt+=1
+        listeVoisinsGraph=[]
 
+def voisinGeo(nbrandom,n):#calcule les 2-4 différents voisins géographiques possibles
+    if (nbrandom==1) : return [2,n+1]                               #4 coins
+    if (nbrandom==n) : return [n-1,2*n]
+    if (nbrandom==n**2-n+1) : return [n**2-2*n+1,n**2-n+2]
+    if (nbrandom==n**2) : return [n**2-n,n**2-1]
+
+    if (1<nbrandom<n) : return [nbrandom-1,nbrandom+1,nbrandom+n]   #4 bords
+    if (n**2-n+1<nbrandom<n**2) : return [nbrandom-1,nbrandom+1,nbrandom-n]
+    if (nbrandom%n==1) : return [nbrandom-n,nbrandom+1,nbrandom+n]
+    if (nbrandom%n==0) : return [nbrandom-n,nbrandom-1,nbrandom+n]
+
+    return [nbrandom-n,nbrandom-1,nbrandom+1,nbrandom+n]            #centre
+
+
+        
+def CoinBordOuCentreLaby(n,nbrandom):#prends un peu de mémoire mais gagne du temps
+    if (nbrandom==1 or nbrandom==n or nbrandom==n**2-n+1 or nbrandom==n**2): return 2
+    if (nbrandom%n==1 or nbrandom%n==0 or 1<nbrandom<n or n**2-n+1<nbrandom<n**2): return 3
+    return 4
 
 
 
