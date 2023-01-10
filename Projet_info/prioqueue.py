d7d6df84ba6118060c52d408358ae1a0cb511900
BOTXavier <xavier.dartevel@gmail.com>
2023-01-10 18:34:31 +0100

    test fenetre pyqt

--- Projet_info/prioqueue.py
+++ Projet_info/prioqueue.py
@@ -2,18 +2,26 @@ import heapq
 
 class PrioQueue(object):
     def __init__(self, l):
-        """Cree une file a priorite a partir d'une liste l :
-        la liste l doit contenir les couples (priorite, element)"""
+        """
+        Créer une file à priorité à partir d'une liste l :
+        la liste l doit contenir les couples (priorité, element)
+        """
         self.queue = [[prio, i, elt] for (i, (prio, elt)) in enumerate(l)]
         heapq.heapify(self.queue)
 
+
     def __repr__(self):
-        """Representation chaine de la file a priorite"""
+        """
+        Représentation chaine de la file a priorite
+        """
         return '<PrioQueue: {0.queue}>'.format(self)
 
+
     def decrease_prio(self, elt, new_prio):
-        """Met a jour la priorite de 'elt' dans la file
-        en la remplacant par 'new_prio'"""
+        """
+        Met à jour la priorite de 'elt' dans la file
+        en la remplaçant par 'new_prio'
+        """
         for i, (_, _, e) in enumerate(self.queue):
             if e == elt:
                 self.queue[i][0] = new_prio
@@ -21,12 +29,18 @@ class PrioQueue(object):
         return self.queue
         raise KeyError("decrease_prio")
 
+
     def pop(self):
-        """Extrait de la file a priorite l'element de priorite minimale
-        Renvoie le couple (priorite, element) correspondant"""
+        """
+        Extrait de la file à priorité l'élement de priorité minimale
+        Renvoie le couple (priorité, élement) correspondant
+        """
         prio, _, elt = heapq.heappop(self.queue)
         return (prio, elt)
 
+
     def is_empty(self):
-        """Teste si la file a priorite est vide"""
+        """
+        Teste si la file à priorité est vide
+        """
         return len(self.queue) == 0
