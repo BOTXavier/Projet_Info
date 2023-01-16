c50cda8f74d04d5675513a57197a2e4d9a47c34d
BOTXavier <xavier.dartevel@gmail.com>
2023-01-16 09:16:52 +0100

    fonctionne

--- Projet_info/main.py
+++ Projet_info/main.py
@@ -6,8 +6,9 @@ import pyplot_lab
 
 def main():
     n = 5
-    G = lg.Laby_Graph(n,1,1) 
     g = lg.laby_dict(n,1) 
+    G = lg.Laby_Graph(n,g,1,1) 
+    
     pyplot_lab.plot_laby(G,g,lab=False) 
     r = dj.dijkstra_classic(G,1,n**2)
     pyplot_lab.plot_soluce(r,n)
